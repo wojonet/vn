71fa1f0a19a4c44dadfeef218b870d5d7c303bfa
Alec Wojciechowski <wojonet@gmail.com>
2026-01-22 22:46:27 -0600

    profress

--- locations/utils/pixelToFeetInchesRatio.js
+++ locations/utils/pixelToFeetInchesRatio.js
@@ -1,15 +1,19 @@
-function caclulateWidth(pixelWidth) {
-    const ratio = pixelWidth / 160
-    const inches = ratio * (11*12 + 10) // 11 feet 10 inches
-    const feet = Math.floor(inches / 12)
-    const remainingInches = Math.round(inches % 12)
-    return `${feet}ft ${remainingInches}in`
-}
+var calculateWidth = function caclulateWidth(pixelWidth) {
+  const ratio = pixelWidth / 160;
+  const inches = ratio * (11 * 12 + 10); // 11 feet 10 inches
+  const feet = Math.floor(inches / 12);
+  const remainingInches = Math.round(inches % 12);
+  return `${feet}ft ${remainingInches}in`;
+};
 
 var calculateHeight = function calculateHeight(pixelHeight) {
-    const ratio = pixelHeight / 210
-    const inches = ratio * (15*12 + 8) // 15 feet 8 inches
-    const feet = Math.floor(inches / 12)
-    const remainingInches = Math.round(inches % 12)
-    return `${feet}ft ${remainingInches}in`
-}
+  const ratio = pixelHeight / 210;
+  const inches = ratio * (15 * 12 + 8); // 15 feet 8 inches
+  const feet = Math.floor(inches / 12);
+  const remainingInches = Math.round(inches % 12);
+  return `${feet}ft ${remainingInches}in`;
+};
+
+HEIGHT = 16;
+
+console.log(calculateHeight(HEIGHT)); // Example usage
