d2406b7322d436173a549f67d717afeaa810a445
Alec Wojciechowski <wojonet@gmail.com>
2026-01-23 11:32:58 -0600

    test

--- locations/utils/pixelToFeetInchesRatio.js
+++ locations/utils/pixelToFeetInchesRatio.js
@@ -1,6 +1,6 @@
 var calculateWidth = function caclulateWidth(pixelWidth) {
   const ratio = pixelWidth / 160;
-  const inches = ratio * (11 * 12 + 10); // 11 feet 10 inches
+  const inches = ratio * ((11 * 12) + 10); // 11 feet 10 inches
   const feet = Math.floor(inches / 12);
   const remainingInches = Math.round(inches % 12);
   return `${feet}ft ${remainingInches}in`;
@@ -8,7 +8,7 @@ var calculateWidth = function caclulateWidth(pixelWidth) {
 
 var calculateHeight = function calculateHeight(pixelHeight) {
   const ratio = pixelHeight / 210;
-  const inches = ratio * (15 * 12 + 8); // 15 feet 8 inches
+  const inches = ratio * ((15 * 12) + 8); // 15 feet 8 inches
   const feet = Math.floor(inches / 12);
   const remainingInches = Math.round(inches % 12);
   return `${feet}ft ${remainingInches}in`;
@@ -16,9 +16,8 @@ var calculateHeight = function calculateHeight(pixelHeight) {
 
 // get command line args
 const args = process.argv.slice(2);
-if (args.length === 2) {
-  const pixelWidth = parseInt(args[0], 10);
-  const pixelHeight = parseInt(args[1], 10);
-  console.log(`Width: ${calculateWidth(pixelWidth)}`);
-  console.log(`Height: ${calculateHeight(pixelHeight)}`);
+if (args.length === 1) {
+  const dimesion = args[0];
+  console.log(`Width: ${calculateWidth(parseInt(dimesion, 10))}`);
+  console.log(`Height: ${calculateHeight(parseInt(dimesion, 10))}`);
 }
